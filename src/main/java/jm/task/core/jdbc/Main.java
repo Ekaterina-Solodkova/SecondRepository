28f3520200d78e882c767c76622a8a9407763000
Ekaterina Solodkova <ek.solodkova02@gmail.com>
2025-01-16 21:22:56 +0300

    Task 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import antlr.Utils;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Maria", "Petrova", (byte) 23);
+        userService.saveUser("Aleksandr", "Smirnov", (byte) 27);
+        userService.saveUser("Victoria", "Sidorova", (byte) 30);
+        userService.saveUser("Dmitry", "Ivanov", (byte) 36);
+        userService.removeUserById(1);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,92 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    private Connection conn = Util.getConnection();
+    private static int id = 1;
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        String sqlCommand = "CREATE TABLE users (Id INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(20), lastName VARCHAR(20), Age TINYINT)";
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate(sqlCommand);
+        } catch (SQLException ex) {
+        }
     }
 
     public void dropUsersTable() {
+        String sqlCommand = "DROP TABLE users";
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate(sqlCommand);
+            id = 1;
+        } catch (SQLException ex) {
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String sql = "INSERT INTO USERS (ID, NAME, LASTNAME, AGE) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            User user = new User(name, lastName, age);
+            user.setId(((long) id++));
+            ps.setLong(1, user.getId());
+            ps.setString(2, name);
+            ps.setString(3, lastName);
+            ps.setInt(4, age);
+            ps.executeUpdate();
+            System.out.println("User с именем — " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM USERS WHERE ID = ?";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM USERS";
+        try (Statement statement = conn.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("ID"));
+                user.setName(resultSet.getString("NAME"));
+                user.setLastName(resultSet.getString("LASTNAME"));
+                user.setAge(resultSet.getByte("AGE"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String sql = "TRUNCATE USERS";
+        try (Statement statement = conn.createStatement()) {
+            statement.execute(sql);
+            id = 1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
