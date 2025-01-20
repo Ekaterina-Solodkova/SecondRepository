547de7039acd508d19a5afc03f309495cc8cc903
Ekaterina Solodkova <ek.solodkova02@gmail.com>
2025-01-20 11:42:52 +0300

    Task 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
@@ -9,34 +12,58 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
+    private static final SessionFactory sessionFactory = Util.getSessionFactory();
 
     @Override
     public void createUsersTable() {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        String sqlCommand = "CREATE TABLE IF NOT EXISTS users (Id INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(20), lastName VARCHAR(20), Age TINYINT)";
+        session.createSQLQuery(sqlCommand).addEntity(User.class).executeUpdate();
+        session.getTransaction().commit();
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        String sqlCommand = "DROP TABLE IF EXISTS users";
+        session.createSQLQuery(sqlCommand).addEntity(User.class).executeUpdate();
+        session.getTransaction().commit();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.save(new User(name, lastName, age));
+        session.getTransaction().commit();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        User user = session.get(User.class, id);
+        session.delete(user);
+        session.getTransaction().commit();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        List<User> users = session.createQuery("from User").getResultList();
+        session.getTransaction().commit();
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createQuery("delete User").executeUpdate();
+        session.getTransaction().commit();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "root";
+
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection conn = null;
@@ -21,4 +29,26 @@ public class Util {
         }
         return conn;
     }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, DB_DRIVER);
+            settings.put(Environment.URL, DB_URL);
+            settings.put(Environment.USER, DB_USERNAME);
+            settings.put(Environment.PASS, DB_PASSWORD);
+
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+            sessionFactory = new Configuration()
+                    .setProperties(settings)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
 }
